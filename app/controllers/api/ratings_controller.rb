62ee7e8ff0fe33163655cb6e44a0fc8d5d85e01a
nicholepatrisse <nichole.patrisse@gmail.com>
2020-06-05 09:21:08 -0800

    Add controller actions

--- app/controllers/api/ratings_controller.rb
+++ app/controllers/api/ratings_controller.rb
@@ -1,13 +1,26 @@
 class Api::RatingsController < ApplicationController
     def create
+        @rating = Rating.new(rating_params)
+
+        if @rating.save
+            render :show
+        else
+            render json: @rating.errors.full_messages, status: 422
+        end
     end
 
     def update
+        @rating = Rating.find(params[:id])
+        @rating.update!(rating_params)
+        render :show
     end
 
     def destroy
+        @rating = Rating.find(params[:id])
+        @rating.destroy!
     end
 
     def rating_params
+        params.require(:rating).permit(:overall, :food, :ambiance, :user_id, :restaurant_id, :reservation_id)
     end
 end
---
+++ db/migrate/20200603215228_add_reservation_to_rating.rb
@@ -0,0 +1,5 @@
+class AddReservationToRating < ActiveRecord::Migration[5.2]
+  def change
+    add_column :ratings, :reservation_id, :integer
+  end
+end
